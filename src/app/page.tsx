7732ded19df184457243c142d9e62225f747f5b7
Shahid Bagwan <112220551+Shahid-Bagwan@users.noreply.github.com>
2024-05-14 15:08:34 +0530

    deployment fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,85 +23,93 @@ const Meeting: React.FC = () => {
   const [videoActive, setVideoActive] = useState(true); // State to manage video track activity
   const [audioActive, setAudioActive] = useState(true);
   useEffect(() => {
-    const joinRoom = async () => {
-      try {
-        const APP_ID: string = "44ac98e9fc0c42e2bcfa9546ff2766d8"; // Replace with your Agora App ID
-        const TOKEN: string | null =
-          "007eJxTYBDeeDJ4Vr60jp2s+aR/ryf3iUxhy5um88hGLeU3Q2uDbZcCg4lJYrKlRaplWrJBsolRqlFSclqipamJWVqakbmZWYpFOpNzWkMgI4PlemtWRgYIBPFZGHITM/MYGACgFBys"; // Replace with your token if you have enabled token security
-        const UID: UID | null = null; // Set UID or null to let Agora assign one
+    if (typeof window !== "undefined") {
+      const joinRoom = async () => {
+        try {
+          const APP_ID: string = "44ac98e9fc0c42e2bcfa9546ff2766d8"; // Replace with your Agora App ID
+          const TOKEN: string | null =
+            "007eJxTYBDeeDJ4Vr60jp2s+aR/ryf3iUxhy5um88hGLeU3Q2uDbZcCg4lJYrKlRaplWrJBsolRqlFSclqipamJWVqakbmZWYpFOpNzWkMgI4PlemtWRgYIBPFZGHITM/MYGACgFBys"; // Replace with your token if you have enabled token security
+          const UID: UID | null = null; // Set UID or null to let Agora assign one
 
-        await client.join(APP_ID, "main", TOKEN, UID);
+          await client.join(APP_ID, "main", TOKEN, UID);
 
-        const audioTrack = await AgoraRTC.createMicrophoneAudioTrack();
-        const videoTrack = await AgoraRTC.createCameraVideoTrack();
-        setLocalAudioTrack(audioTrack);
-        setLocalVideoTrack(videoTrack);
+          const audioTrack = await AgoraRTC.createMicrophoneAudioTrack();
+          const videoTrack = await AgoraRTC.createCameraVideoTrack();
+          setLocalAudioTrack(audioTrack);
+          setLocalVideoTrack(videoTrack);
 
-        videoTrack.play("local-container");
-        await client.publish([audioTrack, videoTrack]);
+          videoTrack.play("local-container");
+          await client.publish([audioTrack, videoTrack]);
 
-        client.on("user-published", async (user, mediaType) => {
-          await client.subscribe(user, mediaType);
-          // Update remote users state
-          setRemoteUsers((prevUsers) => {
-            const newUsers = [...prevUsers];
-            const index = newUsers.findIndex((u) => u.uid === user.uid);
-            if (index === -1) {
-              newUsers.push(user);
+          client.on("user-published", async (user, mediaType) => {
+            await client.subscribe(user, mediaType);
+            // Update remote users state
+            setRemoteUsers((prevUsers) => {
+              const newUsers = [...prevUsers];
+              const index = newUsers.findIndex((u) => u.uid === user.uid);
+              if (index === -1) {
+                newUsers.push(user);
+              }
+              return newUsers;
+            });
+
+            if (mediaType === "video") {
+              const remoteVideoTrack = user.videoTrack as IRemoteVideoTrack;
+              let playerContainer = document.getElementById(
+                user.uid.toString()
+              );
+              if (!playerContainer) {
+                playerContainer = document.createElement("div");
+                playerContainer.id = user.uid.toString();
+                playerContainer.style.width = "320px";
+                playerContainer.style.height = "240px";
+                document
+                  .getElementById("remote-container")
+                  ?.appendChild(playerContainer);
+              }
+              remoteVideoTrack.play(playerContainer);
             }
-            return newUsers;
+
+            if (mediaType === "audio") {
+              user.audioTrack?.play();
+            }
           });
 
-          if (mediaType === "video") {
-            const remoteVideoTrack = user.videoTrack as IRemoteVideoTrack;
-            let playerContainer = document.getElementById(user.uid.toString());
-            if (!playerContainer) {
-              playerContainer = document.createElement("div");
-              playerContainer.id = user.uid.toString();
-              playerContainer.style.width = "320px";
-              playerContainer.style.height = "240px";
-              document
-                .getElementById("remote-container")
-                ?.appendChild(playerContainer);
-            }
-            remoteVideoTrack.play(playerContainer);
-          }
+          client.on("user-unpublished", (user) => {
+            // Remove user from remote users state
+            setRemoteUsers((prevUsers) =>
+              prevUsers.filter((u) => u.uid !== user.uid)
+            );
+            const playerContainer = document.getElementById(
+              user.uid.toString()
+            );
+            playerContainer?.remove();
+          });
 
-          if (mediaType === "audio") {
-            user.audioTrack?.play();
-          }
-        });
+          client.on("user-left", (user) => {
+            setRemoteUsers((prevUsers) =>
+              prevUsers.filter((u) => u.uid !== user.uid)
+            );
+            const playerContainer = document.getElementById(
+              user.uid.toString()
+            );
+            playerContainer?.remove();
+          });
+        } catch (error) {
+          console.log("error", error);
+        }
+      };
 
-        client.on("user-unpublished", (user) => {
-          // Remove user from remote users state
-          setRemoteUsers((prevUsers) =>
-            prevUsers.filter((u) => u.uid !== user.uid)
-          );
-          const playerContainer = document.getElementById(user.uid.toString());
-          playerContainer?.remove();
-        });
+      joinRoom();
 
-        client.on("user-left", (user) => {
-          setRemoteUsers((prevUsers) =>
-            prevUsers.filter((u) => u.uid !== user.uid)
-          );
-          const playerContainer = document.getElementById(user.uid.toString());
-          playerContainer?.remove();
-        });
-      } catch (error) {
-        console.log("error", error);
-      }
-    };
-
-    joinRoom();
-
-    return () => {
-      localAudioTrack?.stop();
-      localAudioTrack?.close();
-      localVideoTrack?.stop();
-      localVideoTrack?.close();
-      client.leave();
-    };
+      return () => {
+        localAudioTrack?.stop();
+        localAudioTrack?.close();
+        localVideoTrack?.stop();
+        localVideoTrack?.close();
+        client.leave();
+      };
+    }
   }, []);
   const toggleVideo = () => {
     if (localVideoTrack) {
